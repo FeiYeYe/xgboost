27640bf1cd0afb4c46d4009d656da2c3d0e0295e
SummerZheng <candyzyn@gmail.com>
2016-04-15 11:31:41 -0500

    fix can't handle missing = NA bug

--- R-package/R/utils.R
+++ R-package/R/utils.R
@@ -109,7 +109,6 @@ xgb.get.DMatrix <- function(data, label = NULL, missing = NULL, weight = NULL) {
     if (is.null(label)) {
       stop("xgboost: need label when data is a matrix")
     }
-    dtrain <- xgb.DMatrix(data, label = label)
     if (is.null(missing)){
       if (is.null(weight)) {
         dtrain <- xgb.DMatrix(data, label = label)
